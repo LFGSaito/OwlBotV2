1b9c1d52e6fd2371ac5983667b4bd38024e4a0d0
SaitoH <LFGSaito@users.noreply.github.com>
2021-04-09 22:49:47 -0400

    time time time

--- exts/ratinthekitchen.py
+++ exts/ratinthekitchen.py
@@ -1,12 +1,12 @@
 import discord
 import discord.ext.commands as commands
 import datetime
-from time import gmtime, strftime
 import common as cmn
+import time
 
 datetimeFormat = '%Y-%m-%d %H:%M:%S'
 date2 = '2019-11-18 12:25:34'
-date1 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+date1 = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
 
 
@@ -17,6 +17,7 @@ class RatCog(commands.Cog):
 
 @commands.command(name="ratinthekitchen", aliases=["rat"], category=cmn.Cats.FUN)
 async def rat(ctx: commands.Context):
+    """Checks for Rats"""
     rats = f"No rats spotted in the caf as of today, if this changes DM Saito, time since last seen {diff}"
     await ctx.send(rats)
     return
