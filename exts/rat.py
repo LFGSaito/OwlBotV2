d31399885d166a64e87d5a5c4ba0912697e3a54a
SaitoH <LFGSaito@users.noreply.github.com>
2021-04-09 22:16:25 -0400

    setup

--- exts/rat.py
+++ exts/rat.py
@@ -10,8 +10,17 @@ date1 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
 
 
+class RatCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
 @commands.command(name="rat", aliases=["r"], category=cmn.Cats.FUN)
 async def rat(command: str, message: discord.Message):
     rats = f"No rats spotted in the caf as of today, if this changes DM Saito, time since last seen {diff}"
     await message.channel.send(rats)
     return
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(RatCog(bot))
