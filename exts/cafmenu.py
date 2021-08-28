28fa5b7a218ce9f0d997d18d1fcc362ed5806106
SaitoH <2722492+saitohirga@users.noreply.github.com>
2021-08-27 23:53:15 -0400

    Update cafmenu.py

--- exts/cafmenu.py
+++ exts/cafmenu.py
@@ -8,7 +8,19 @@ class MenuCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(name="cafmenu", aliases=["caf"], category=cmn.Cats.LOOKUP)
+    @commands.command(name="cafmenu breakfast", aliases=["caf b"], category=cmn.Cats.LOOKUP)
+    async def _Caf(self, ctx: commands.Context):
+        
+        await ctx.send(str(rats))
+        return
+
+    @commands.command(name="cafmenu lunch", aliases=["caf l"], category=cmn.Cats.LOOKUP)
+    async def _Caf(self, ctx: commands.Context):
+        
+        await ctx.send(str(rats))
+        return
+
+    @commands.command(name="cafmenu dinner", aliases=["caf d"], category=cmn.Cats.LOOKUP)
     async def _Caf(self, ctx: commands.Context):
         
         await ctx.send(str(rats))
