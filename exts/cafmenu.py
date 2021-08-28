7d61a0df18862ba8ce460ebc36ed69446a09e6a3
SaitoH <2722492+saitohirga@users.noreply.github.com>
2021-08-27 23:40:37 -0400

    yeet

--- exts/cafmenu.py
+++ exts/cafmenu.py
@@ -1,25 +1,19 @@
 import discord.ext.commands as commands
-import datetime
 import common as cmn
-import time
-
-datetimeFormat = '%Y-%m-%d %H:%M:%S'
-date2 = '2019-11-18 12:25:34'
-date1 = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
 
 
-class RatCog(commands.Cog):
+
+
+class MenuCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(name="ratinthekitchen", aliases=["rat"], category=cmn.Cats.FUN)
-    async def _rat(self, ctx: commands.Context):
-        """Checks for Rats"""
-        rats = f"No rats spotted in the caf as of today, if this changes DM Saito, time since last seen {diff}"
+    @commands.command(name="cafmenu", aliases=["caf"], category=cmn.Cats.LOOKUP)
+    async def _Caf(self, ctx: commands.Context):
+        
         await ctx.send(str(rats))
         return
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(RatCog(bot))
+    bot.add_cog(MenuCog(bot))
