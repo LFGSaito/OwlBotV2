4abf7fe0e4d3b6bc18d57c3a975cb7e040fdeb56
SaitoH <2722492+saitohirga@users.noreply.github.com>
2021-09-13 09:33:16 -0400

    error handling?

--- exts/ratinthekitchen.py
+++ exts/ratinthekitchen.py
@@ -3,7 +3,11 @@ import datetime
 import common as cmn
 import time
 
-difference = "TBD"
+datetimeFormat = '%Y-%m-%d %H:%M:%S'
+date2 = '2019-11-18 12:25:34'
+date1 = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
+
 
 class RatCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -12,8 +16,8 @@ class RatCog(commands.Cog):
     @commands.command(name="ratinthekitchen", aliases=["rat"], category=cmn.Cats.FUN)
     async def _rat(self, ctx: commands.Context):
         """Checks for Rats"""
-        rats = f"No rats spotted in the caf as of today, if this changes DM Saito, time since last seen {difference}"
-        await ctx.send(str(rats))
+        rats = f"No rats spotted in the caf as of today, if this changes DM Saito, time since last seen {diff}"
+        await ctx.send("DateTime Format is" + datetimeFormat + "Date2 is" + date2 + "date 1 is" + date1 + "and Diff is" + diff + rats)
         return
 
 
